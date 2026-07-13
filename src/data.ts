5aa919d7fa63ae73035fbe7cf7b683c1bc41f55d
Eugene-Eyram-Ahiavey <phostechservices@gmail.com>
2026-07-13 07:34:08 +0000

    images changed

--- src/data.ts
+++ src/data.ts
@@ -1,27 +1,27 @@
 
 export const celebrant = {
-  name: "Lovia",
-  dob: "20001-07-13",  
+    name: "Lovia",
+    dob: "20001-07-13",
 };
 
 export const messages = [
-  "Happy Birthday to the most amazing person!",
-  "May your day be filled with joy and laughter.",
-  "Thank you for being such a wonderful human.",
-  "You deserve every beautiful thing life has to offer.", 
-]; 
+    "Happy Birthday to the most amazing person!",
+    "May your day be filled with joy and laughter.",
+    "Thank you for being such a wonderful human.",
+    "You deserve every beautiful thing life has to offer.",
+];
 
 export const photos = [
-  "/images/mobile-lovia1.jpeg",
-  "/images/mobile-lovia2.jpeg",
-  "/images/mobile-lovia3.jpeg",
-  "/images/mobile-lovia5.jpeg",
-  "/images/mobile-lovia6.jpg",
-  "/images/mobile-lovia7.jpg",
-  "/images/mobile-lovia8.jpg",
-  "/images/mobile-lovia9.jpg",
-  "/images/mobile-lovia10.jpg",
-  "/images/mobile-lovia12.jpg",
-  "/images/mobile-lovia13.jpg",
-  "/images/mobile-lovia14.jpg"
+    "/images/mobile-lovia1.jpeg",
+    "/images/mobile-lovia2.jpeg",
+    "/images/mobile-lovia3.jpeg",
+    "/images/mobile-lovia5.jpeg",
+    "/images/mobile-lovia6.jpg",
+    "/images/mobile-lovia7.JPG",
+    "/images/mobile-lovia8.jpg",
+    "/images/mobile-lovia9.jpg",
+    "/images/mobile-lovia10.jpg",
+    "/images/mobile-lovia12.jpg",
+    "/images/mobile-lovia13.jpg",
+    "/images/mobile-lovia14.jpg"
 ];
